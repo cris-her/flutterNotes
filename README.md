20c4a0d05dece003c15536121518acaf134bdf6a
chernandez <chernandez@hasar.com.mx>
2023-02-25 00:33:52 -0600

    hw

---
+++ README.md
@@ -0,0 +1,1 @@
+# flutterNotes
--- main.dart
+++ main.dart
@@ -2,63 +2,212 @@ import 'package:flutter/material.dart';
 
 main() {
   runApp(HolaMundo());
-  // runapp - arranque
-  // estado = dinamismo de la pantalla
-  // stateless no cambia ... comportamiento de la pantalla
-  // statefull si se va a interactuar, se divide en 2 less y full
-  // 3. material design y cupertino style -> estilo
-  // layout o distribucion
-  // resto de widgets
-
-  // floating button en stateless no jalaria
 }
-// andamio -> scaffold , distibucion, plantilla
-// appbar, leader, title, actions
-// tabs
-// drawer - cajonera
-// end drawer
-// body
-// floating action button
-// bottom navigation bar
 
-// ctrl + / para comentar
-
-//logcat take screen
 class HolaMundo extends StatelessWidget {
   @override
-  // vs cuppertino style
-  // vs custom scaffold
   Widget build(BuildContext context) => MaterialApp(
-        // ctrl + p
-        // ctrl + clic
-        // ctrl + alt + l
-        // child - children -> lista de widgets // acá se suele resolver si tiene la propiedad
-        // alt + enter
         debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text("Hola mundo"),
-            // alfa
-            backgroundColor: Color.fromARGB(255, 7, 94, 84),
-            actions: [Icon(Icons.search)],
-          ),
-          // fractional size
-          // trailing colillaa
-            body: ListView(children: [
-              ListTile(leading: CircleAvatar(backgroundImage: NetworkImage("https://catingtonpost.com/wp-content/uploads/2016/10/Like-a-Boss-funny-meme-01.jpg")), title: Text("fresa"), subtitle: Text("sub"),),
-              Divider(),
-              ListTile(title: Text("uva")),
-              ListTile(title: Text("manzana")),
-              ListTile(title: Text("sandia"))
-            ],),
-            //CircleAvatar(backgroundImage: NetworkImage("https://catingtonpost.com/wp-content/uploads/2016/10/Like-a-Boss-funny-meme-01.jpg")),
-            // Center(child: Image.network("https://catingtonpost.com/wp-content/uploads/2016/10/Like-a-Boss-funny-meme-01.jpg", width: 300,)),
-
-
-
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {},
-            child: Icon(Icons.add_a_photo),
+        home: DefaultTabController(
+          length: 4,
+          initialIndex: 1,
+          child: Scaffold(
+            appBar: AppBar(
+              title: Text("WhatsApp"),
+              bottom: TabBar(
+                isScrollable: true,
+                indicatorColor: Colors.white,
+                labelColor: Colors.white,
+                unselectedLabelColor: Color.fromARGB(255, 179, 217, 210),
+                labelPadding: EdgeInsets.symmetric(horizontal: 16.0),
+                labelStyle: TextStyle(fontSize: 16),
+                tabs: <Widget>[
+                  Tab(
+                    icon: Icon(
+                      Icons.people,
+                      size: 22,
+                    ),
+                  ),
+                  Tab(
+                    text: "   Chats   ",
+                  ),
+                  Tab(
+                    text: " Estados ",
+                  ),
+                  Tab(
+                    text: "Llamadas",
+                  )
+                ],
+              ),
+              backgroundColor: Color.fromARGB(255, 0, 128, 105),
+              actions: [
+                Padding(
+                  padding: EdgeInsets.only(right: 10.0, left: 11.0),
+                  child: Icon(Icons.camera_alt_outlined),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(right: 10.0, left: 11.0),
+                  child: Icon(Icons.search),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(right: 10.0, left: 11.0),
+                  child: Icon(Icons.more_vert),
+                )
+              ],
+            ),
+            body: ListView(
+              children: [
+                ListTile(
+                  leading: CircleAvatar(
+                      radius: 22,
+                      backgroundImage: NetworkImage(
+                          "https://raw.githubusercontent.com/cris-her/ZipTest/master/ProfFelipe.PNG")),
+                  title: Stack(
+                    children: [
+                      Text(
+                        "Prof Felipe",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      Positioned(
+                        top: 0,
+                        right: 0,
+                        child: Text(
+                          "7:56 p. m.",
+                          style: TextStyle(fontSize: 12),
+                        ),
+                      ),
+                    ],
+                  ),
+                  subtitle: Text("Cristofer Joshua Hernández Sánchez"),
+                ),
+                ListTile(
+                  leading: CircleAvatar(
+                      radius: 22,
+                      backgroundImage: NetworkImage(
+                          "https://raw.githubusercontent.com/cris-her/ZipTest/master/Ico.PNG")),
+                  title: Stack(
+                    children: [
+                      Text(
+                        "GEN-2020",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      Positioned(
+                        top: 0,
+                        right: 0,
+                        child: Text(
+                          "7:53 p. m.",
+                          style: TextStyle(fontSize: 12),
+                        ),
+                      ),
+                    ],
+                  ),
+                  subtitle: Text("+52 55 6802 5796: Ay, si ya ví"),
+                ),
+                ListTile(
+                  leading: CircleAvatar(
+                      radius: 22,
+                      backgroundImage: NetworkImage(
+                          "https://raw.githubusercontent.com/cris-her/ZipTest/master/ServicioSocial.PNG")),
+                  title: Stack(
+                    children: [
+                      Text(
+                        "Servicio social Aaron",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      Positioned(
+                        top: 0,
+                        right: 0,
+                        child: Text(
+                          "10:22 a. m.",
+                          style: TextStyle(fontSize: 12),
+                        ),
+                      ),
+                    ],
+                  ),
+                  subtitle: Text("+52 56 1492 9371: Los que no lo han..."),
+                ),
+                ListTile(
+                  leading: CircleAvatar(
+                      radius: 22,
+                      backgroundImage: NetworkImage(
+                          "https://raw.githubusercontent.com/cris-her/ZipTest/master/Fes.PNG")),
+                  title: Stack(
+                    children: [
+                      Text(
+                        "FES Aragón",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      Positioned(
+                        top: 0,
+                        right: 0,
+                        child: Text(
+                          "22/02/23",
+                          style: TextStyle(fontSize: 12),
+                        ),
+                      ),
+                    ],
+                  ),
+                  subtitle: Text("Hola, buenas noches, te compartimos..."),
+                ),
+                ListTile(
+                  leading: CircleAvatar(
+                      radius: 22,
+                      backgroundImage: NetworkImage(
+                          "https://raw.githubusercontent.com/cris-her/ZipTest/master/Ale.PNG")),
+                  title: Stack(
+                    children: [
+                      Text(
+                        "Ale",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      Positioned(
+                        top: 0,
+                        right: 0,
+                        child: Text(
+                          "10:50 a. m.",
+                          style: TextStyle(fontSize: 12),
+                        ),
+                      ),
+                    ],
+                  ),
+                  subtitle: Text("¿Tienes las llaves del A203?"),
+                ),
+                ListTile(
+                  leading: CircleAvatar(
+                      radius: 22,
+                      backgroundImage: NetworkImage(
+                          "https://raw.githubusercontent.com/cris-her/ZipTest/master/Elon.PNG")),
+                  title: Stack(
+                    children: [
+                      Text(
+                        "Elon Musk",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      Positioned(
+                        top: 0,
+                        right: 0,
+                        child: Text(
+                          "2:26 p. m.",
+                          style: TextStyle(fontSize: 12),
+                        ),
+                      ),
+                    ],
+                  ),
+                  subtitle: Text("Acabo de comprar un planeta xD"),
+                ),
+              ],
+            ),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () {},
+              child: Icon(Icons.chat),
+              backgroundColor: Color.fromARGB(255, 0, 128, 105),
+            ),
           ),
         ),
       );
