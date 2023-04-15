f7139e7d1369567982d056522d3c898df795c68b
chernandez <crs.hdz.s@gmail.com>
2023-04-14 21:07:47 -0600

    1404

--- main.dart
+++ main.dart
@@ -3,11 +3,29 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+
+
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  //const MyApp({super.key});
+  TextEditingController txtNum1 = TextEditingController();
+  TextEditingController txtNum2 = TextEditingController();
+
+  int total = 0;
+  bool dividir = false;
+  bool sumar = false;
+
+  String grupoSexo="";
+  String sexo = "";
+
+  double calificacion = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -17,31 +35,67 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
-          body: GridView.extent(
-        scrollDirection: Axis.horizontal,
-        maxCrossAxisExtent: 300.0,
-        padding: EdgeInsets.all(10.0),
-        mainAxisSpacing: 25,
-        crossAxisSpacing: 50,
-        children: [
-          Image.network(
-            "https://www.shutterstock.com/image-photo/portrait-funny-cat-sunglasses-260nw-195698564.jpg",
-            height: 180,
-          ),
-          Image.network(
-            "https://www.shutterstock.com/image-photo/portrait-funny-cat-sunglasses-260nw-195698564.jpg",
-            height: 180,
-          ),
-          Image.network(
-            "https://www.shutterstock.com/image-photo/portrait-funny-cat-sunglasses-260nw-195698564.jpg",
-            height: 180,
-          ),
-          Image.network(
-            "https://www.shutterstock.com/image-photo/portrait-funny-cat-sunglasses-260nw-195698564.jpg",
-            height: 180,
-          ),
-        ],
-      )),
+          body: Padding(
+            padding: EdgeInsets.all(12.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+
+                TextField(
+                  controller: txtNum1,
+                  decoration: InputDecoration(
+                      label: Text("Numero 1"),
+                      hintText: "Ingresa un numero",
+                      border: OutlineInputBorder(borderRadius: BorderRadius.circular(15))
+                  ),
+                ),
+                TextField(
+                  controller: txtNum2,
+                  decoration: InputDecoration(
+                      label: Text("Numero 2"),
+                      hintText: "Ingresa otro numero",
+                      border: OutlineInputBorder(borderRadius: BorderRadius.circular(15))
+                  ),
+                ),
+                CheckboxListTile(title: Text("Dividir?"),value: dividir, onChanged: (value){
+                  setState(() {
+                    dividir = value!;
+                  });
+                }),
+                SwitchListTile(title: Text("Sumar?"),value: sumar, onChanged: (value){
+                  setState(() {
+                    sumar = value!;
+                  });
+                }),
+                RadioListTile(title: Text("hombre"),value: "hombre", groupValue: grupoSexo, onChanged: (value){
+                  setState(() {
+                    grupoSexo = value.toString()!;
+                  });
+                }),
+                RadioListTile(title: Text("mujer"),value: "mujer", groupValue: grupoSexo, onChanged: (value){
+                  setState(() {
+                    grupoSexo = value.toString()!;
+                  });
+                }),
+                Slider(activeColor: Colors.deepPurple,label: calificacion.toString(), min: 0, max: 10, divisions: 10,value: calificacion, onChanged: (value){
+                  setState(() {
+                    calificacion = value;
+                    print(calificacion);
+                  });
+                }),
+
+                SizedBox(height: 15),
+                ElevatedButton(onPressed: (){
+                  setState(() {
+                    total = int.parse(txtNum1.text) * int.parse(txtNum2.text);
+                  });
+                  print(total);
+                }, child: Text("Multiplicar")),
+                Text("El resultado es: ${total}", style: TextStyle(fontSize: 20),)
+              ],
+            )
+          )
+      ),
     );
   }
 }
@@ -78,7 +132,27 @@ class CustomClipPath extends CustomClipper<Path> {
 
   @override
   bool shouldReclip(CustomClipper<Path> oldClipper) => true;
+
+
 }
+
+
+/*
+Stack(
+          children: [
+            Image.network(
+              "https://www.shutterstock.com/image-photo/portrait-funny-cat-sunglasses-260nw-195698564.jpg",
+              height: 100,
+            ),
+            Positioned(left: 20, top: 30,child: Image.network(
+              "https://www.shutterstock.com/image-photo/portrait-funny-cat-sunglasses-260nw-195698564.jpg",
+              height: 50,
+            ))
+          ],
+      )
+*/
+
+
 /*
 
         String conValor = "value";
